9f019f16305725da603c6bc9f7149068828c49ff
manikagnish <72827130+manikagnish@users.noreply.github.com>
2022-01-01 19:35:25 +0530

    created login form

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,3 +1,4 @@
+import { FaSignInAlt, FaSignOutAlt } from "react-icons/fa";
 import Link from "next/link";
 import styles from "@/styles/Header.module.css";
 import Search from "./Search";
@@ -23,6 +24,13 @@ export default function Header() {
           <li>
             <Link href="/events/add">
               <a>Add Events</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/account/login">
+              <a className="btn-icon btn-secondary">
+                <FaSignInAlt /> Login
+              </a>
             </Link>
           </li>
         </ul>
