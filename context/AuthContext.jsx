999bb86246966b42d9091ed803c33f26cee9d462
manikagnish <72827130+manikagnish@users.noreply.github.com>
2022-01-04 19:48:25 +0530

    persisted active user from cookie

--- context/AuthContext.jsx
+++ context/AuthContext.jsx
@@ -7,6 +7,11 @@ const AuthContext = createContext();
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [error, setError] = useState(null);
+  const router = useRouter();
+
+  useEffect(() => {
+    checkUserLoggedIn();
+  }, []);
 
   // Register user
   const register = async (user) => {
@@ -27,6 +32,7 @@ export const AuthProvider = ({ children }) => {
 
     if (res.ok) {
       setUser(data.user);
+      router.push("/account/dashboard");
     } else {
       setError(data.message);
       setError(null);
@@ -40,7 +46,14 @@ export const AuthProvider = ({ children }) => {
 
   // Check if user is logged in
   const checkUserLoggedIn = async (user) => {
-    console.log("Check");
+    const res = await fetch(`${NEXT_URL}/api/user`);
+    const data = await res.json();
+
+    if (res.ok) {
+      setUser(data.user);
+    } else {
+      setUser(null);
+    }
   };
 
   return (
---
+++ pages/account/dashboard.jsx
@@ -0,0 +1,5 @@
+import Layout from "@/components/Layout";
+
+export default function dashboard() {
+  return <Layout title="User Dashboard">dashboard</Layout>;
+}
