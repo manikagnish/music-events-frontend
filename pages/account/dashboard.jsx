936a156f97e80ee48ae88fcaba4425000e9d2bc6
manikagnish <72827130+manikagnish@users.noreply.github.com>
2022-01-08 16:30:10 +0530

    Added DashboardEvent component

--- pages/account/dashboard.jsx
+++ pages/account/dashboard.jsx
@@ -1,5 +1,43 @@
+import { parseCookies } from "@/helpers/index";
+import { API_URL } from "@/config/index";
 import Layout from "@/components/Layout";
+import styles from "@/styles/Dashboard.module.css";
+import DashboardEvent from "@/components/DashboardEvent";
 
-export default function dashboard() {
-  return <Layout title="User Dashboard">dashboard</Layout>;
+export default function dashboard({ events }) {
+  const deleteEvent = (id) => {
+    console.log("delete event", id);
+  };
+
+  return (
+    <Layout title="User Dashboard">
+      <div className={styles.dash}>
+        <h1>Dashboard</h1>
+        <h3>My Events</h3>
+
+        {events.map((evt) => (
+          <DashboardEvent key={evt.id} evt={evt} handleDelete={deleteEvent} />
+        ))}
+      </div>
+    </Layout>
+  );
+}
+
+export async function getServerSideProps({ req }) {
+  const { token } = parseCookies(req);
+
+  const res = await fetch(`${API_URL}/events/me`, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  const events = await res.json();
+
+  return {
+    props: {
+      events,
+    },
+  };
 }
